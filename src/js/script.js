732c55708d1c6c0d3bf482587854781ddf185767
jhandzel130696 <79660573+jhandzel130696@users.noreply.github.com>
2021-05-27 18:06:57 +0200

    Working server API with one endpoint

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,11 @@
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
   
   const templates = {
@@ -518,12 +523,28 @@
       const thisApp = this;
       //console.log('thisApp.data',thisApp.data);
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
     initData: function(){
       const thisApp=this;
-      thisApp.data = dataSource;},
+      thisApp.data = {};
+      const url = settings.db.url+'/'+ settings.db.products;
+
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse',parsedResponse);
+
+          //save parsedResponse as thisApp.data.products
+          thisApp.data.products=parsedResponse;
+          //execute initMenu method
+          thisApp.initMenu();
+        });
+      console.log('thisApp.data',JSON.stringify(thisApp.data));
+    },
     init: function(){
       const thisApp = this;
       //console.log('*** App starting ***');
@@ -532,7 +553,7 @@
       //console.log('settings:', settings);
       // console.log('templates:', templates);
       thisApp.initData();
-      thisApp.initMenu();
+      
       thisApp.initCart();
     },
     initCart(){
