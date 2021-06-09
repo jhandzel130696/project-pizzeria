019c3263a25c1f47c0174739013502f4ccf7e257
Jan Handzel <jhandzel130696@gmail.com>
2021-06-09 17:04:52 +0200

    working class booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,10 +1,11 @@
-import {templates} from '../settings.js';
-
+import {templates,select} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
 class Booking{
   constructor(widgetContainerElement){
     const thisBooking=this;
     thisBooking.render(widgetContainerElement);
     thisBooking.initWidgets();
+    
   }
 
   render(widgetContainerElement){
@@ -13,7 +14,21 @@ class Booking{
     thisBooking.dom={};
     thisBooking.dom.wrapper=widgetContainerElement;
     thisBooking.dom.wrapper.innerHTML=generatedHTML;
+    thisBooking.dom.peopleAmount=document.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount=document.querySelector(select.booking.hoursAmount);
+    //console.log('peopleAmount',thisBooking.dom.peopleAmount);
+  }
 
+  initWidgets(){
+    const thisBooking=this;
+    thisBooking.peopleAmountWidget= new AmountWidget(thisBooking.dom.peopleAmount);
+    
+
+    thisBooking.hoursAmountWidget= new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.wrapper.addEventListener('change',function(){
+
+    });
+    
   }
 
 
